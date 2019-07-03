13bdb78d77861dc057e2af04978bf17dca9350e2
Cristian Espinosa <crespinosa@s.sfusd.edu>
2019-07-03 10:40:41 -0700

    smooth movement

--- Demo Day Project/Assets/PlayerController.cs
+++ Demo Day Project/Assets/PlayerController.cs
@@ -2,35 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public float speed = 20f;
+
 public class PlayerController : MonoBehaviour
 {
+
+    Rigidbody2D rb2d;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+    private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-
-        if (Input.GetKey("w"))
-        {
-            pos.z += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.z -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= speed * Time.deltaTime;
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 targetVelocity = new Vector2(moveHorizontal * 10f, moveVertical * 10f);
+        rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
     }
+
 }
